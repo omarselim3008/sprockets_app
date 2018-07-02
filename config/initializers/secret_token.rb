501eab415706c80e84020d58af5a6c0bcd86bd61
Omar Selim <omar@intellectionsoftware.com>
2018-07-02 16:33:12 +0100

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SprocketsApp::Application.config.secret_token = 'cdc4286197589da15868c480b955dedf2d0068071c7cbc8d291004eb93d4bc79e6a2fbc52790cd1d524411d2152294a639fb32e250c340e0ece075f522cdc6e9'
